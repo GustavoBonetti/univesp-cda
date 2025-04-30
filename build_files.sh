fc8a63f2f5fd2dc8a856b5f4d63e9e0455a66729
Gustavo Bonetti <gustavobonetti8@gmail.com>
2025-04-30 11:19:56 -0300

    change static root folder and python version

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,7 @@
 echo "BUILD START"
-python3.9 -m ensurepip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 -m ensurepip
+echo "pip install"
+python3.10 -m pip install -r requirements.txt
+echo "manage.py"
+python3.10 manage.py collectstatic --noinput --clear
 echo "BUILD END"
--- cda/settings.py
+++ cda/settings.py
@@ -123,7 +123,7 @@ br_formats.DATE_FORMAT = 'd/m/Y'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
