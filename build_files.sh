eab2233bad4068900c9a43445e5d6f453bbdde08
Gustavo Bonetti <gustavobonetti8@gmail.com>
2025-04-30 11:56:10 -0300

    change vercel.json

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,6 @@ echo "BUILD START"
 python3.9 -m ensurepip
 echo "pip install"
 python3.9 -m pip install -r requirements.txt
-#echo "manage.py"
-#python3.9 manage.py collectstatic --noinput --clear
+echo "manage.py"
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
